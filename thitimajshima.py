4c1c0222c823763e554c1154860b2050e5e1035a
Alexander Steshenko <CorvoOrc@users.noreply.github.com>
2015-04-01 21:59:56 +0300

    Update and rename THITIMAJSHIMA.py to thitimajshima.py

--- thitimajshima.py
+++ thitimajshima.py
@@ -1,4 +1,7 @@
+__author__ = 'Steshenko'
+
 # -*- coding: cp1251 -*-
+
 import PIL.Image as Image
 import PIL.ImageDraw as ImageDraw
 import matplotlib.pyplot as plt
@@ -8,14 +11,16 @@ import pylab
 import math
 import random
 
-def embeding_subband(data, w_b, w_e, h_b, h_e, coeff):
+
+def embeding_subband(data, w_b, w_e, h_b, h_e, coef):
     k, l = 0, 0
     for i in xrange(h_b, h_e):
         for j in xrange(w_b, w_e):
-            data[i][j], l = coeff[k][l], l + 1
+            data[i][j], l = coef[k][l], l + 1
         k, l = k + 1, 0
-        
+
     return data
+
 
 def draw_edge(data, w, h, level):
     W, H = w, h
@@ -23,57 +28,60 @@ def draw_edge(data, w, h, level):
         for i in xrange(W):
             data[H/2][i] = -1
         H, W = H/2, W/2
-        
+
     W, H = w, h
     for k in xrange(level):
         for i in xrange(H):
             data[i][W/2] = -1
         H, W = H/2, W/2
-
     return data
+
 
 def embeding_subbands(w, h, coeffs, level):
     LL1, (LH1, HL1, HH1) = coeffs
     matrixCoeffs = [[0 for j in xrange(w)] for i in xrange(h)]
-    
     matrixCoeffs = embeding_subband(matrixCoeffs, 0,    w/2,  0,    h/2,  LL1)
     matrixCoeffs = embeding_subband(matrixCoeffs, 0,    w/2,  h/2,  h,    LH1)
     matrixCoeffs = embeding_subband(matrixCoeffs, w/2,  w,    0,    h/2,  HL1)
     matrixCoeffs = embeding_subband(matrixCoeffs, w/2,  w,    h/2,  h,    HH1)
-
     matrixCoeffs = draw_edge(matrixCoeffs, w, h, level)
-    
     return matrixCoeffs
+
 
 def PSNR(img, img_):
     return 10 * math.log10(250**2 * (len(img) * len(img[0])) / sum(sum((img - img_)**2)))
 
+
 def MSE(img, img_):
     return sum(sum((img - img_)**2)) / (len(img) * len(img[0]))
 
+
 def SR(img, img_):
-    S, D = 0.0, 0.0
+    s, d = 0.0, 0.0
     for i in xrange(len(img)):
         for j in xrange(len(img[i])):
-            if int(img[i][j]) == int(img_[i][j]): S += 1
-            else: D += 1
+            if int(img[i][j]) == int(img_[i][j]):
+                s += 1
+            else: d += 1
 
-    return S / (S + D)
+    return s / (s + d)
+
 
 def pdf(x):
-    return (1 / (math.sqrt(2*math.pi))) * math.exp(- (x**2) / 2)
+    return (1 / (math.sqrt(2*math.pi))) * math.exp(-(x**2) / 2)
 
 wavelet = pywt.Wavelet('db4')
-level = 2    # lwl decomposition
-mode = 'sym' # protected mode
+level = 2
+mode = 'sym'
 
-name = raw_input('Enter path to image:')
+name = 'hill.png'  # raw_input('Enter path to image:')
 image = Image.open(name)
 print 'Mode open image -', image.mode
-if not image.mode in 'LIF': image = image.convert('L')
+if not (image.mode in 'LIF'):
+    image = image.convert('L')
 width, height = image.size
 image = numpy.array(image)
-image_ = Image.open('/Atack/Peppers/WMI_Gausse.png').convert('L')
+image_ = Image.open('cat.jpg').convert('L')
 image_ = numpy.array(image_)
 
 coeffs = pywt.wavedec2(image, wavelet, mode, level)
@@ -83,30 +91,36 @@ C1, C2 = 0.0, 0.0
 
 for i in ch1:
     for j in i:
-        if j > C1: C1 = j
+        if j > C1:
+            C1 = j
 for i in cv1:
     for j in i:
-        if j > C1: C1 = j
+        if j > C1:
+            C1 = j
 for i in cd1:
     for j in i:
-        if j > C1: C1 = j
+        if j > C1:
+            C1 = j
 
 for i in ch2:
     for j in i:
-        if j > C2: C2 = j
+        if j > C2:
+            C2 = j
 for i in cv2:
     for j in i:
-        if j > C2: C2 = j
+        if j > C2:
+            C2 = j
 for i in cd2:
     for j in i:
-        if j > C2: C2 = j
+        if j > C2:
+            C2 = j
 
 T1 = 2 ** (int(math.log(C1, 2)) - 1)
 T2 = 2 ** (int(math.log(C2, 2)) - 1)
-
-print 'T1, T2, C1, C2 =', T1, T2, C1, C2
+print 'T1={t1} T2={t2} C1={c1} C2={c2}'.format(t1=T1, t2=T2, c1=C1, c2=C2)
 PRS = []
-for i in xrange(2000): PRS.append(pdf(random.gauss(0, 1)))
+for i in xrange(2000):
+    PRS.append(pdf(random.gauss(0, 1)))
 
 alp, x = 0.2, 0
 for i in xrange(len(ch1)):
@@ -142,22 +156,21 @@ for i in xrange(len(cv2)):
         if cv2[i][j] > T2:
             cv2[i][j] += alp * abs(cv2[i][j]) * PRS[x]
             x += 1
-            
+
 alp, x = 0.2, 0
 for i in xrange(len(cd2)):
     for j in xrange(len(cd2[i])):
         if cd2[i][j] > T2:
             cd2[i][j] += alp * abs(cd2[i][j]) * PRS[x]
             x += 1
-            
+
 coeffs = ca2, (ch2, cv2, cd2), (ch1, cv1, cd1)
 WatermarkedImg = pywt.waverec2(coeffs, wavelet)
 
 iris = WatermarkedImg - image
 psnr = PSNR(image, WatermarkedImg)
 mse = MSE(image, WatermarkedImg)
-print 'psnr =', psnr
-print 'mse =', mse
+print 'PSNR={psnr}\nMSE={mse}'.format(psnr=psnr, mse=mse)
 
 ii = Image.new('L', (len(WatermarkedImg), len(WatermarkedImg[0])))
 for i in xrange(len(WatermarkedImg)):
@@ -166,40 +179,44 @@ for i in xrange(len(WatermarkedImg)):
 
 ii.save('WMI.png')
 
-###### Watermark Detection
-
+# Watermark Detection
 coeffs = pywt.wavedec2(WatermarkedImg, wavelet, mode, level)
-#coeffs = pywt.wavedec2(image_, wavelet, mode, level)
 ca2, (ch2, cv2, cd2), (ch1, cv1, cd1) = coeffs
 
 C1, C2 = 0.0, 0.0
-
 for i in ch1:
     for j in i:
-        if j > C1: C1 = j
+        if j > C1:
+            C1 = j
 for i in cv1:
     for j in i:
-        if j > C1: C1 = j
+        if j > C1:
+            C1 = j
 for i in cd1:
     for j in i:
-        if j > C1: C1 = j
+        if j > C1:
+            C1 = j
 
 for i in ch2:
     for j in i:
-        if j > C2: C2 = j
+        if j > C2:
+            C2 = j
 for i in cv2:
     for j in i:
-        if j > C2: C2 = j
+        if j > C2:
+            C2 = j
 for i in cd2:
     for j in i:
-        if j > C2: C2 = j
+        if j > C2:
+            C2 = j
 
 T1 = 2 ** (int(math.log(C1, 2)) - 1)
 T2 = 2 ** (int(math.log(C2, 2)) - 1)
 
-print '\nT1, T2, C1, C2 =', T1, T2, C1, C2
+print 'T1={t1} T2={t2} C1={c1} C2={c2}'.format(t1=T1, t2=T2, c1=C1, c2=C2)
 PRS = []
-for i in xrange(2000): PRS.append(pdf(random.gauss(0, 1)))
+for i in xrange(2000):
+    PRS.append(pdf(random.gauss(0, 1)))
 
 z, S = 0.0, 0.0
 alp, y = 0.2, 0
@@ -245,7 +262,7 @@ for i in xrange(len(cv2)):
             z += abs(cv2[i][j]) * PRS[y]
             S += abs(cv2[i][j])
             y += 1
-            
+
 M += y
 y = 0
 for i in xrange(len(cd2)):
@@ -255,20 +272,22 @@ for i in xrange(len(cd2)):
             S += abs(cd2[i][j])
             y += 1
 M += y
-z = z / M
-S = S * (alp / (2 * M))
+z /= M
+S *= (alp / (2*M))
 
-print 'z, S =', z, S
-if z > S: print 'Watermark present'
-else: print 'Watermark absent'
+print 'z={z} S={S}'.format(z=z, S=S)
+if z > S:
+    print 'Watermark present'
+else:
+    print 'Watermark absent'
 
 plt.figure(1)
 plt.title('Original image')
-plt.imshow(image, cmap = plt.get_cmap('gray'))
+plt.imshow(image, cmap=plt.get_cmap('gray'))
 plt.figure(2)
 plt.title('Watermarked image')
-plt.imshow(WatermarkedImg, cmap = plt.get_cmap('gray'))
+plt.imshow(WatermarkedImg, cmap=plt.get_cmap('gray'))
 plt.figure(3)
 plt.title('Subtract')
-plt.imshow(iris, cmap = plt.get_cmap('gray'))
+plt.imshow(iris, cmap=plt.get_cmap('gray'))
 pylab.show()
